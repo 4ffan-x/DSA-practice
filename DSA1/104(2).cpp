ff876977fd75caeed9a5449ffd00660240b0c612
affandhanbad-dev <affandhanbad@gmail.com>
2025-12-13 16:22:22 +0530

    .

--- DSA1/104(2).cpp
+++ DSA1/104(2).cpp
@@ -13,6 +13,23 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if (root == nullptr) return 0;
-        return 1 + max(maxDepth(root->left),maxDepth(root->right));
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            depth++;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        return depth;
     }
 };
