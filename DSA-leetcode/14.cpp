320d0e135ff682fc990b0e24ace16b60cbd319e4
affandhanbad-dev <affandhanbad@gmail.com>
2025-12-25 15:53:14 +0530

    fixing

--- DSA-leetcode/14.cpp
+++ DSA-leetcode/14.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string ans = "";
-        int s = strs.size();
         sort(strs.begin(),strs.end());
         int i = 0;
-        while (i<s && strs[0][i] == strs[s-1][i]){
+        while (i<strs[0].size() && strs[0][i] == strs[strs.size()-1][i]){
             ans += strs[0][i];
             i++;
         }
