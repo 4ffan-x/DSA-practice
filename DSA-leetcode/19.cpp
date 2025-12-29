193b12ab5642f514b7d9611f988b1e5baf5ea03a
affandhanbad-dev <affandhanbad@gmail.com>
2025-12-29 13:38:00 +0530

    .

---
+++ DSA-leetcode/19.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* fast = head;
+        ListNode* slow = dummy;
+
+        for(int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        while(fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        ListNode* del = slow->next;
+        slow->next = del->next;
+        delete del;
+
+        ListNode* newHead = dummy->next;
+        delete dummy;
+
+        return newHead;
+    }
+};
