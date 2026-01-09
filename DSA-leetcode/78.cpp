1d7a7bf2722338c958f10f54f34ae7eec11054e2
affandhanbad-dev <affandhanbad@gmail.com>
2026-01-09 22:03:05 +0530

    .

---
+++ DSA-leetcode/78.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> temp;
+
+    void backtrack(vector<int>& nums, int index) {
+        result.push_back(temp);
+        for (int i = index; i < nums.size(); i++) {
+            temp.push_back(nums[i]);
+            backtrack(nums, i + 1);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtrack(nums, 0);
+        return result;
+    }
+};
