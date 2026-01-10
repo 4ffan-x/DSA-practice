2d9870c8eb2f7bbabea676d4f6d890c87c7e6024
affandhanbad-dev <affandhanbad@gmail.com>
2026-01-10 21:01:04 +0530

    .

---
+++ DSA-leetcode/225.cpp
@@ -0,0 +1,33 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+        // rotate previous elements behind x
+        for(int i = 0; i < sz - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
